830ed2c33621437c7a068ebee2a94a66598e907f
Ankit Shah <ankit.tronix@gmail.com>
2018-09-26 10:47:59 -0400

    Updating SVM

--- SVM.py
+++ SVM.py
@@ -3,6 +3,6 @@ from sklearn import svm
 
 class SVM(Classifier):
     def buildClassifier(self,X_features,Y_features):
-        clf= svm.SVC(kernel='linear').fit(X_features,Y_features)
+        clf= svm.LinearSVC(C=0.0003).fit(X_features,Y_features)
         #clf= svm.SVC(kernel='rbf').fit(X_features,Y_features)
         return clf
