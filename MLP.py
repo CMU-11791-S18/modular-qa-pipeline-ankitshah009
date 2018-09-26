5736bfd7a9691a8afb5af66a600cd686f78e2426
Ankit Shah <ankit.tronix@gmail.com>
2018-09-26 17:57:36 -0400

    Updating hiddne states

--- MLP.py
+++ MLP.py
@@ -3,7 +3,7 @@ from sklearn.neural_network import MLPClassifier
 
 class MLP(Classifier):
     def buildClassifier(self,X_features,Y_features):
-    	clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1).fit(X_features,Y_features)
+    	clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(50, 10), random_state=1).fit(X_features,Y_features)
         #clf= svm.SVC(kernel='linear').fit(X_features,Y_features)
         #clf= svm.SVC(kernel='rbf').fit(X_features,Y_features)
         return clf
