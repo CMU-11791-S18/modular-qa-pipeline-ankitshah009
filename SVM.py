30741d38064ef1620b4ea628c0ab0ad126c779dc
Ankit Shah <ankit.tronix@gmail.com>
2018-09-26 17:59:55 -0400

    Updating svm

--- SVM.py
+++ SVM.py
@@ -4,7 +4,7 @@ from sklearn.multiclass import OneVsRestClassifier
 
 class SVM(Classifier):
     def buildClassifier(self,X_features,Y_features):
-        clf= OneVsRestClassifier(svm.LinearSVC(C=0.0003),n_jobs=20)
-	clf.fit(X_features,Y_features)
+        #clf= OneVsRestClassifier(svm.LinearSVC(C=0.0003),n_jobs=20)
+	clf=svm.LinearSVC(C=0.0004).fit(X_features,Y_features)
         #clf= svm.SVC(kernel='rbf').fit(X_features,Y_features)
         return clf
