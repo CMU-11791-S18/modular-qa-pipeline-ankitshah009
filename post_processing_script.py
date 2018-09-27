b686fc79a3a0203c7d530df40743b25ef9b24cd6
Ankit Shah <ankit.tronix@gmail.com>
2018-09-26 23:55:44 -0400

    Printing classes as well

--- post_processing_script.py
+++ post_processing_script.py
@@ -7,6 +7,11 @@ count_easy=0
 count_improvement=0
 count_regression=0
 
+count_tough_dict={}
+count_easy_dict={}
+count_improvement_dict={}
+count_regression_dict={}
+
 with open(sys.argv[1],'rb') as f:
 	gt_val = pickle.load(f)
 
@@ -20,19 +25,43 @@ for i in range(len(gt_val)):
 	if val_pred_model_S[i] == gt_val[i]:
 		if val_pred_model_S_dash[i] == gt_val[i]:
 			count_easy+=1
+			if str(gt_val[i]) not in count_easy_dict:
+				count_easy_dict[str(gt_val[i])]=1
+			else:
+				count_easy_dict[str(gt_val[i])]+=1
 		if val_pred_model_S_dash[i] != gt_val[i]:
 			count_regression+=1
+			if str(gt_val[i]) not in count_regression_dict:
+				count_regression_dict[str(gt_val[i])]=1
+			else:
+				count_regression_dict[str(gt_val[i])]+=1
 	else:
 		if val_pred_model_S_dash[i] == gt_val[i]:
 			count_improvement+=1
+			if str(gt_val[i]) not in count_improvement_dict:
+				count_improvement_dict[str(gt_val[i])]=1
+			else:
+				count_improvement_dict[str(gt_val[i])]+=1
 		if val_pred_model_S_dash[i] != gt_val[i]:
 			count_tough+=1
+			if str(gt_val[i]) not in count_tough_dict:
+				count_tough_dict[str(gt_val[i])]=1
+			else:
+				count_tough_dict[str(gt_val[i])]+=1
+def print_dict(dict_obj):
+	for keys,values in dict_obj.items():
+		print(keys)
+		print(values)
 print "Count Easy"
 print (count_easy)
+print_dict(count_easy_dict)
 print "Count Tough"
 print (count_tough)
+print_dict(count_tough_dict)
 print "Count Improvement"
 print (count_improvement)
+print_dict(count_improvement_dict)
 print "Count Regression"
 print (count_regression)
+print_dict(count_regression_dict)
 
