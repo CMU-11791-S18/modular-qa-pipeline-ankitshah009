5be3cb1c1b072a6f15c27ddcb85e03a42ece9fa8
Ankit Shah <ankit.tronix@gmail.com>
2018-09-26 21:39:47 -0400

    Updating codes

--- TfidfFeaturizer.py
+++ TfidfFeaturizer.py
@@ -8,7 +8,7 @@ class TfidfFeaturizer(Featurizer):
 
 	#The abstract method from the base class is implemeted here to return count features
 	def getFeatureRepresentation(self, X_train, X_val):
-		tfidf_vect = TfidfVectorizer(smooth_idf=False,min_df=5)
+		tfidf_vect = TfidfVectorizer(smooth_idf=True,min_df=20,max_df=2000)
 		X_train_counts = tfidf_vect.fit_transform(X_train)
 		X_val_counts = tfidf_vect.transform(X_val)
 		return X_train_counts, X_val_counts
